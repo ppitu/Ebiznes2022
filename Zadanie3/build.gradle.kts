fb80b1e47355c2bbaee8d0dc827b9b04a4cc04ff
Piotrek <piotr.chmura.1998@o2.pl>
2022-04-22 18:21:48 +0200

    Add zadanie3

---
+++ Zadanie3/build.gradle.kts
@@ -0,0 +1,42 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val slack_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.20"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-client-auth:$ktor_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
+    implementation("dev.kord:kord-core:0.7.4")
+    implementation ("com.slack.api:slack-api-client:$slack_version")
+    // Add these dependencies if you want to use the Kotlin DSL for building rich messages
+    implementation ("com.slack.api:slack-api-model-kotlin-extension:$slack_version")
+    implementation ("com.slack.api:slack-api-client-kotlin-extension:$slack_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
